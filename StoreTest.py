4adfbc1a6cf8050a7dbea80a59d8a5a1ef2789ab
Leon Gersen <leongersen@gmail.com>
2015-05-12 11:44:26 +0200

    Added chunked reading

--- StoreTest.py
+++ StoreTest.py
@@ -14,24 +14,24 @@ SER.send('Endint at: %s. Sector: %s\n' % (MOD.secCounter(), sector))
 
 SER.send('Done initialize\n\n\n')
 
-SER.send('Sector: %s\n' % sector)
-SER.send('ReadSector: %s\n' % Storage.readSector)
-SER.send('activeSector: %s\n' % Storage.activeSector)
-
-Storage.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam facilisis elit vitae ex mollis consequat. Nunc eu sapien ipsum. Morbi urna sapien, dapibus eu lectus eget, malesuada pretium arcu. Praesent vehicula mi eu mauris consectetur, sit amet sodales nisi rhoncus. Donec eu sollicitudin mi. Sed et lacus congue sem iaculis tincidunt. Donec nec lorem ac velit commodo ullamcorper. Suspendisse nulla felis, sodales at sodales vitae, scelerisque a urna. Suspendisse mollis eros non risus semper porttitor. Quisque quis nunc ornare purus massa nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam facilisis elit vitae ex mollis consequat. ")
-
-Storage.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ac ligula vitae nibh placerat ullamcorper ut id ipsum. Nunc vehicula ligula quis ex lacinia, et viverra arcu posuere. Fusce pretium sapien odio, in blandit sapien efficitur eget. Aliquam sagittis imperdiet nunc vel convallis. Duis fermentum nisl et vestibulum suscipit. Nullam eget orci eleifend, euismod massa non, molestie turpis. Vivamus tortor elit, tempor vel urna at, semper laoreet nibh. Phasellus tristique efficitur metus vitae suscipit. Praesent eget nisi at massa cras amet. Nunc vehicula ligula quis ex lacinia, et viverra arcu posuere. ")
-
-SER.send('ReadSector: %s\n' % Storage.readSector)
-SER.send('activeSector: %s\n' % Storage.activeSector)
-SER.send('--------------------\n')
+Storage.readSector = 102
 
 # Can't do assignment in condition
 while 1:
 	data = Storage.read()
+
 	if data == 0:
 		break
-	SER.send(data)
-	SER.send('\n\n\n')
+
+	dataLen = len(data)
+
+	# To prevent overflowing the serial buffer,
+	# Chunk the result if it is overly long.
+	if ( dataLen > 1000 ):
+		SER.send(data[0:1000])
+		MOD.sleep(1)
+		SER.send(data[1000:])
+	else:
+		SER.send(data)
 
 SER.send('\n\nEnd\n')
