a54cda67afa805d0eb63027c7b95a40113df6e3d
Leon Gersen <leongersen@gmail.com>
2015-05-13 10:10:27 +0200

    Init to functions, config support

--- Main.py
+++ Main.py
@@ -24,19 +24,25 @@ SER.send('Imported Storage\n')
 
 SER.send('Done importing\n')
 
+# Builds message from peripherals
 def getComposedMessage ( ):
-	return '["%s"]' % GPS.getActualPosition()
+	return '["%s"]' % GPS.getActualPosition() # TODO add gauge, and current sense
 
-def updateSettings ( response ):
+# Write settings to config
+def updateSettings ( line ):
+	SER.send('Updating settings to: ')
+	SER.send(line)
+	SER.send('\n')
 
-	SER.send(response)
+	settings = line.split(',')
 
-	# Push to Fix mode, interval. Use setting from control panel;
-	#GPS.powerSavingMode(2, Config.IntervalGPS)
+	# settings[0] is 'reserved'
+	Config.Mode = settings[1]
+
 
 # Throws all messages since boot on the serial port
 def logOut ( ):
-	
+
 	SER.send('\n\n<LOG>')
 
 	while 1:
@@ -55,6 +61,9 @@ def logOut ( ):
 		else:
 			SER.send(message)
 
+		#SER.send('\n')
+		#MOD.sleep(5)
+
 	SER.send('</LOG>\n\n')
 
 # Stores a message
@@ -67,10 +76,10 @@ def transmitMessage ( message ):
 	# If the socket is not open, we'll dial.
 	if Module.socketIsSuspended() == 0:
 
-		SER.send('Dail socket')
+		SER.send('Dail socket\n')
 
 		if Module.socketDail( Config.API ) == 0:
-			pass # Failed to open a socket.
+			return 1 # Failed to open a socket.
 
 	elif Module.socketResume() == 0:
 		SER.send('Failed socket resume\n')
@@ -78,77 +87,106 @@ def transmitMessage ( message ):
 	response = Module.makeRequest("/", message)
 
 	if ( response == 0 ):
-		pass # The request failed.
+		return 1 # The request failed.
 	else:
 		updateSettings(response)
 
 	if ( Module.sendEscapeSequence() == 0 ):
-		pass # Failed to escape, not in command mode.
+		return 1 # Failed to escape, not in command mode.
 
+	return 0
 
+# Configure general device settings
+def initSettings ( ):
+	# Set error reporting to numeric;
+	Module.enableErrorReporting()
+	SER.send('Done enableErrorReporting\n')
 
+	# Don't send the (+++) escape sequence when suspending a socket;
+	Module.skipEscape()
+	SER.send('Done skipEscape\n')
 
+	# Flow control is not connected;
+	Module.disableFlowControl()
+	SER.send('Done disableFlowControl\n')
 
-# Set error reporting to numeric;
-Module.enableErrorReporting()
-SER.send('Done enableErrorReporting\n')
+	# Test module only;
+	Module.activeGPSAntenna()
+	SER.send('Done activeGPSAntenna\n')
 
-# Don't send the (+++) escape sequence when suspending a socket;
-Module.skipEscape()
-SER.send('Done skipEscape\n')
+# Attach to the GPRS network
+def initNetworkRelated ( ):
 
-# Unlock SIM card by entering PIN;
-Module.unlockSIM()
-SER.send('Done unlockSIM\n')
+	# Unlock SIM card by entering PIN;
+	Module.unlockSIM()
+	SER.send('Done unlockSIM\n')
 
-# Flow control is not connected;
-Module.disableFlowControl()
-SER.send('Done disableFlowControl\n')
+	if ( Module.attachNetwork() == 0 ):
+		SER.send('Failed attachNetwork\n')
+	SER.send('Done attachNetwork\n')
 
-# Test module only;
-Module.activeGPSAntenna()
-SER.send('Done activeGPSAntenna\n')
+	if ( Module.connectNetwork( Config.APN ) == 0 ):
+		SER.send('Failed connectNetwork\n')
+	SER.send('Done connectNetwork\n')
 
-if ( Module.attachNetwork() == 0 ):
-	SER.send('Failed attachNetwork\n')
-SER.send('Done attachNetwork\n')
-
-if ( Module.connectNetwork( Config.APN ) == 0 ):
-	SER.send('Failed connectNetwork\n')
-SER.send('Done connectNetwork\n')
-
-SER.send('Starting initialization at: %s\n' % MOD.secCounter())
-
-sector = Storage.initialize()
-SER.send('End at: %s. Sector: %s\n' % (MOD.secCounter(), sector))
-
-Module.CPUclock(0) # Clock back to default (@26Mhz)
-SER.send('CPU back down\n')
-
-count = 0
-
-while 1:
+# Read the serial port, see if the user wants something
+def acceptCommandInput ( ):
 
 	received = SER.read()
 
 	SER.send("Read: %s\n" % received)
 
 	if received.find('QUIT') == 0:
-		break
+		return 1
 	elif received.find('LOG') == 0:
 		Module.CPUclock(3)
 		logOut()
 		Module.CPUclock(0)
+	elif received.find('CONFIG') == 0:
+		updateSettings(received[7:])
 
+	return 0
+
+# Calls initialization
+def setup ( ):
+	initSettings()
+	initNetworkRelated()
+
+	SER.send('Starting storage initialization at: %s\n' % MOD.secCounter())
+	sector = Storage.initialize()
+	SER.send('End at: %s. Sector: %s\n' % (MOD.secCounter(), sector))
+
+	Module.CPUclock(0) # Clock back to default (@26Mhz)
+	SER.send('CPU back down\n')
+
+setup()
+
+
+count = 0
+
+while 1:
+
+	if acceptCommandInput():
+		break
+
+	# Get message
 	message = getComposedMessage()
 	SER.send("Message: %s\n" % message)
-
 	SER.send('Message count: %s\n' % count)
-	storeMessage(message)
 	count = count + 1
 
-	#transmitMessage(message)
+	SER.send('Mode is: %s\n' % Config.Mode)
 
-	MOD.sleep(5);
+	if Config.Mode == 'Active':
+		transmitMessage(message)
+	elif Config.Mode == 'Passive':
+		storeMessage(message)
+	elif Config.Mode == 'Buffered':
+		SER.send('Buffered mode, todo!\n')
+
+	MOD.sleep(5) # TODO config
+	# Go to sleep here
+
+
 
 SER.send('Stopping execution\n')
