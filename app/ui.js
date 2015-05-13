0c24ed9236c05587331338301f4a341a323446ba
Leon Gersen <leongersen@gmail.com>
2015-05-13 10:11:03 +0200

    Mode control pannel

--- app/ui.js
+++ app/ui.js
@@ -5,3 +5,14 @@
 	menu.addEventListener('click', function(){
 		nav.classList.toggle('visible');
 	});
+
+	var modeSelectionName = document.getElementById('modeSelectionName'),
+		modeSelectionPannel = document.getElementById('modeSelectionPannel'),
+		modeSelectionClose = document.getElementById('modeSelectionClose');
+
+	function toggleModeSelection ( ) {
+		modeSelectionPannel.classList.toggle('visible');
+	}
+
+	modeSelectionName.addEventListener('click', toggleModeSelection);
+	modeSelectionClose.addEventListener('click', toggleModeSelection);
