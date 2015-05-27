92eaabef388679952ffa5fa6a09e4c6ee9121aa2
Leon Gersen <leongersen@gmail.com>
2015-05-27 10:45:33 +0200

    Change provider settings

--- Config.py
+++ Config.py
@@ -1,11 +1,11 @@
 # Network APN
-APN = "smartsites.t-mobile"
+APN = "internet"
 
 # API location (host only)
 API = "track.refreshless.com"
 
 # Transmission mode
-Mode = 'Active'
+Mode = 'Passive'
 
 # Interval to get data from the GPS module (in 100ms)
 Interval = 10
